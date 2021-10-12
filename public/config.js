f6d96ea949a96b6ac7d5fc86d1308f7d72c50479
Steven Riggs <s.riggs@uky.edu>
2021-10-12 08:49:48 -0400

    Add debug and complete url

--- public/config.js
+++ public/config.js
@@ -1,3 +1,4 @@
-window.REACT_APP_API_URL = "http://localhost:8080";
+window.REACT_APP_API_URL = "http://localhost:8080/v1/tasks";
 window.REACT_APP_NAME = "Tactics";
 window.REACT_APP_VERSION = "0.0.0-rc0";
+window.REACT_APP_DEBUG = true;
