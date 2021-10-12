072b9c345fb512ef4856c14b096b02f664624e5e
Steven Riggs <s.riggs@uky.edu>
2021-10-12 08:20:59 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.REACT_APP_API_URL = "http://localhost:5000";
-window.REACT_APP_NAME = "Tactics (DEV)";
+window.REACT_APP_API_URL = "http://localhost:8080";
+window.REACT_APP_NAME = "Tactics";
 window.REACT_APP_VERSION = "0.0.0-rc0";
