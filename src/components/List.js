69fe154ad230d9f9c877f1369bd5ad05aaf726d3
Steven Riggs <s.riggs@uky.edu>
2021-10-12 09:12:13 -0400

    Better layout

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,7 @@
 import React from "react";
 // Third party.
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const queryClient = new QueryClient();
 
@@ -23,9 +24,24 @@ function TaskTable() {
 
   return (
     <div>
-      {data.tasks.map((task) => {
-        return <p>{task.title}</p>;
-      })}
+      <ul class="list-group">
+        {data.tasks.map((task) => {
+          return (
+            <li class="list-group-item">
+              <div>
+                {!task.isCompleted && <i className="bi bi-circle mr-3"></i>}
+                {task.isCompleted && (
+                  <i className="bi bi-check-circle mr-3"></i>
+                )}
+                <strong>{task.title}</strong>
+              </div>
+              <small className="font-weight-light text-muted">
+                {task.description}
+              </small>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
